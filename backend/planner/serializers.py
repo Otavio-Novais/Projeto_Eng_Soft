bcc64e98f88473dc5026de014b4a02ee5a0fc940
Iago Lima <limsamuel812@gmail.com>
2025-11-28 13:26:19 -0300

    Validação de datas

--- backend/planner/serializers.py
+++ backend/planner/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from .models import Trip, Activity, Expense
 from django.contrib.auth.models import User
+from django.utils import timezone
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
@@ -38,8 +39,23 @@ class TripSerializer(serializers.ModelSerializer):
             'description': {'required': False}
         }
 
-        def validate(self, data):
+    def validate(self, data):
+    
+        start_date = data.get('start_date')
+        end_date = data.get('end_date')
+        
+        # Validação de datas
+        if start_date and end_date:
+            
+            if start_date > end_date:
+                raise serializers.ValidationError({
+                    "end_date": "A data final deve ser posterior à data de início."
+                })
 
-            if data['start_date'] > data['end_date']:
-                    raise serializers.ValidationError("A data final deve ser posterior à data de início.")
-            return data
+            # Verificação se a data está no passado
+            if start_date < timezone.now().date():
+                raise serializers.ValidationError({
+                    "start_date": "Não é possível criar uma viagem no passado."
+                })
+
+        return data
